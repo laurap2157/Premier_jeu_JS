4f90cc0c0a46454629e1e3a56c647b235becdede
LauraP2157 <laurapetit2171@gmail.com>
2025-12-23 17:25:51 +0100

    finalisation fonction permettant au chasseur de gagner des points en cliquant - premiers essais fonction déplacement

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,8 @@
 var temps = document.querySelector('#timer');
 var donald = document.querySelector('#donald');
+var secondes = 0;
 
 function timer () {
-    var secondes = 0;
     var pointsDonald = 0;
     var time = setInterval (() => {
         secondes++;
@@ -22,14 +22,59 @@ function timer () {
 
 var monTimer = timer();
 
+// La fonction suivante permettra au chasseur de gagner des points à chaque clic sur Donald, uniquement pendant les 120 secondes que dure la partie//
+
 var chasseur = document.querySelector('#hunter');
-var points = 0;
+var perso = document.querySelector('#perso');
+var pointsChasseur = 0;
 
 function toggleScore () {
-    points ++;
-    chasseur.innerText = points;
+    if (secondes < 120) {
+        pointsChasseur ++;
+        chasseur.innerText = pointsChasseur;
+    }
 }    
 
-donald.addEventListener("click", toggleScore);
+perso.addEventListener("click", toggleScore);
 
-var pointsHunter = toggleScore();
+// La fonction suivante permettra au joueur contrôlant Donald de le déplacer //
+
+var speed = 1;
+perso.style.top = '0vh'; //vh et vw : importants pour pouvoir s'adapter aux différentes tailles d'écran (ce n'est pas parfait... j'espère apprendre des choses à la correction :))//
+perso.style.left = '0vw';
+
+document.addEventListener('keydown', function(e) {
+    var top = parseInt(perso.style.top);
+    var left = parseInt(perso.style.left);
+
+    if(e.key == 'ArrowRight') {
+        left = left + speed;
+        if(left >= 76) {
+            left = 76;
+    0    }
+    }
+
+    if(e.key == 'ArrowLeft') {
+        left = left - speed;
+        if(left <= -13) {
+            left = -13;
+        }
+    }
+
+    if(e.key == 'ArrowUp') {
+        top = top - speed;
+        if(top <= 0) {
+            top = 0;
+        }
+    }
+
+    if(e.key == 'ArrowDown') {
+        top = top + speed;
+        if(top >= 68) {
+            top = 68;
+        }
+    }
+
+    perso.style.top = top + 'vh';
+    perso.style.left = left + 'vw';
+});
